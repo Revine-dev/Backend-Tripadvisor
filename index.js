a989e4ff891e962b91c848dee09db4fa5730daa0
Rempowa <rempowa@MBP-de-Rempowa.home>
2021-04-23 16:15:05 +0100

    initial

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const formidableMiddleware = require("express-formidable");
+const cors = require("cors");
+
+const app = express();
+app.use(formidableMiddleware());
+app.use(cors());
+require("dotenv").config();
+
+const mailgun = require("mailgun-js")({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN,
+});
+
+app.post("/form", (req, res) => {
+  const data = {
+    from: `${req.fields.firstName} ${req.fields.lastName} <${req.fields.email}> `,
+    to: process.env.MAIL,
+    subject: `Formulaire de ${req.fields.email}`,
+    text: `${req.fields.message}----------\n\nMessage envoyé depuis le site : Tripadvisor Copie - Netlify\nhttps://awesome-goldberg-1da884.netlify.app/`,
+  };
+
+  let result = false,
+    errorMessage = "Unknow error";
+
+  try {
+    mailgun.messages().send(data, (error, response) => {
+      if (!error) {
+        result = true;
+      } else {
+        errorMessage = {
+          error: error,
+          response: response,
+        };
+      }
+    });
+  } catch (error) {
+    errorMessage = error.message;
+  }
+
+  res.json(result ? { result: false, error: errorMessage } : { result: true });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
---
+++ main.js
@@ -0,0 +1,44 @@
+const $ = document;
+
+$.addEventListener("DOMContentLoaded", () => {
+  console.log("Page chargée");
+
+  //   $.addEventListener("click", () => {
+  //     console.log("Click !!!");
+  //   });
+
+  //   Bouton "Afficher le text"
+  $.querySelector("#btn-text-display").addEventListener("click", () => {
+    $.querySelector(".hidden").classList.add("display");
+    // $.querySelector("p").classList.remove("hidden");
+  });
+
+  //   Bouton "Cacher le text"
+  $.querySelector("#btn-text-hidden").addEventListener("click", () => {
+    $.querySelector(".hidden").classList.remove("display");
+  });
+
+  //   Bouton "Afficher OU cacher le text"
+  $.querySelector("#btn-text").addEventListener("click", () => {
+    $.querySelector(".hidden").classList.toggle("display");
+  });
+
+  //   Envoyer les données du formulaire
+  $.querySelector("#contact-form").addEventListener("submit", async (event) => {
+    // la fonction preventDefault() annule le comportement par défaut du formulaire (le rafraîchissement de la page)
+    event.preventDefault();
+
+    const data = {
+      firstName: $.querySelector("#firstname").value,
+      lastName: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      message: $.querySelector("#message").value,
+    };
+
+    console.log(data);
+
+    const response = await axios.post("http://localhost:3000/form", data);
+    console.log(response);
+    alert("Merci pour votre formulaire");
+  });
+});
